22f060730a72b5eac8286a7617de67b20be78ed8
koch-andreas <koch-andreas@users.noreply.github.com>
2025-07-01 07:42:46 +0000

    deploy: MeltPoolDG/MeltPoolDG-dev@b2af04ecee849d4ba4ca79c9dcfd083d04b38fe5

--- doxygen/search/classes_7.js
+++ doxygen/search/classes_7.js
@@ -4,6 +4,7 @@ var searchData=
   ['gaussvolumetricintensityprofile_1',['GaussVolumetricIntensityProfile',['../classMeltPoolDG_1_1Heat_1_1GaussVolumetricIntensityProfile.html',1,'MeltPoolDG::Heat']]],
   ['genericdataout_2',['GenericDataOut',['../classMeltPoolDG_1_1GenericDataOut.html',1,'MeltPoolDG']]],
   ['ghostpenaltydata_3',['GhostPenaltyData',['../structMeltPoolDG_1_1GhostPenaltyData.html',1,'MeltPoolDG']]],
-  ['gusarovdata_4',['GusarovData',['../structMeltPoolDG_1_1Heat_1_1LaserData_1_1GusarovData.html',1,'MeltPoolDG::Heat::LaserData']]],
-  ['gusarovintensityprofile_5',['GusarovIntensityProfile',['../classMeltPoolDG_1_1Heat_1_1GusarovIntensityProfile.html',1,'MeltPoolDG::Heat']]]
+  ['ghostpenaltydata_3c_20number_20_3e_4',['GhostPenaltyData&lt; Number &gt;',['../structMeltPoolDG_1_1GhostPenaltyData.html',1,'MeltPoolDG']]],
+  ['gusarovdata_5',['GusarovData',['../structMeltPoolDG_1_1Heat_1_1LaserData_1_1GusarovData.html',1,'MeltPoolDG::Heat::LaserData']]],
+  ['gusarovintensityprofile_6',['GusarovIntensityProfile',['../classMeltPoolDG_1_1Heat_1_1GusarovIntensityProfile.html',1,'MeltPoolDG::Heat']]]
 ];
