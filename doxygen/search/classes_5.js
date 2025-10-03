601494049e6dca9e7b5afb7a2f9c9f7ebc7709f0
mschreter <mschreter@users.noreply.github.com>
2025-10-03 16:26:17 +0000

    deploy: MeltPoolDG/MeltPoolDG-dev@1a10e5f1386f0d9ea0053660e42ce2bf8e83f752

--- doxygen/search/classes_5.js
+++ doxygen/search/classes_5.js
@@ -8,13 +8,14 @@ var searchData=
   ['evaporationmodelbase_5',['EvaporationModelBase',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationModelBase.html',1,'MeltPoolDG::Evaporation']]],
   ['evaporationmodelconstant_6',['EvaporationModelConstant',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationModelConstant.html',1,'MeltPoolDG::Evaporation']]],
   ['evaporationmodelhardtwondra_7',['EvaporationModelHardtWondra',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationModelHardtWondra.html',1,'MeltPoolDG::Evaporation']]],
-  ['evaporationmodelrecoilpressure_8',['EvaporationModelRecoilPressure',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationModelRecoilPressure.html',1,'MeltPoolDG::Evaporation']]],
-  ['evaporationmodelsaturatedvaporpressure_9',['EvaporationModelSaturatedVaporPressure',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationModelSaturatedVaporPressure.html',1,'MeltPoolDG::Evaporation']]],
-  ['evaporationsourcetermsbase_10',['EvaporationSourceTermsBase',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationSourceTermsBase.html',1,'MeltPoolDG::Evaporation']]],
-  ['evaporationsourcetermscontinuous_11',['EvaporationSourceTermsContinuous',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationSourceTermsContinuous.html',1,'MeltPoolDG::Evaporation']]],
-  ['evaporationsourcetermssharp_12',['EvaporationSourceTermsSharp',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationSourceTermsSharp.html',1,'MeltPoolDG::Evaporation']]],
-  ['evaporationthicknessintegrationdata_13',['EvaporationThicknessIntegrationData',['../structMeltPoolDG_1_1Evaporation_1_1EvaporationThicknessIntegrationData.html',1,'MeltPoolDG::Evaporation']]],
-  ['evaporativecooling_14',['EvaporativeCooling',['../classMeltPoolDG_1_1Evaporation_1_1EvaporativeCooling.html',1,'MeltPoolDG::Evaporation']]],
-  ['exactsolution_15',['exactsolution',['../classMeltPoolDG_1_1Simulation_1_1AdvectionDiffusionDG_1_1ExactSolution.html',1,'MeltPoolDG::Simulation::AdvectionDiffusionDG::ExactSolution&lt; dim, number &gt;'],['../classMeltPoolDG_1_1Simulation_1_1ReinitCircle_1_1ExactSolution.html',1,'MeltPoolDG::Simulation::ReinitCircle::ExactSolution&lt; dim, number &gt;'],['../classMeltPoolDG_1_1Simulation_1_1ReinitCircleDG_1_1ExactSolution.html',1,'MeltPoolDG::Simulation::ReinitCircleDG::ExactSolution&lt; dim, number &gt;']]],
-  ['externalfluidforcesrighthandsidecontribution_16',['ExternalFluidForcesRightHandSideContribution',['../structMeltPoolDG_1_1Flow_1_1ExternalFluidForcesRightHandSideContribution.html',1,'MeltPoolDG::Flow']]]
+  ['evaporationmodelknight_8',['EvaporationModelKnight',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationModelKnight.html',1,'MeltPoolDG::Evaporation']]],
+  ['evaporationmodelrecoilpressure_9',['EvaporationModelRecoilPressure',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationModelRecoilPressure.html',1,'MeltPoolDG::Evaporation']]],
+  ['evaporationmodelsaturatedvaporpressure_10',['EvaporationModelSaturatedVaporPressure',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationModelSaturatedVaporPressure.html',1,'MeltPoolDG::Evaporation']]],
+  ['evaporationsourcetermsbase_11',['EvaporationSourceTermsBase',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationSourceTermsBase.html',1,'MeltPoolDG::Evaporation']]],
+  ['evaporationsourcetermscontinuous_12',['EvaporationSourceTermsContinuous',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationSourceTermsContinuous.html',1,'MeltPoolDG::Evaporation']]],
+  ['evaporationsourcetermssharp_13',['EvaporationSourceTermsSharp',['../classMeltPoolDG_1_1Evaporation_1_1EvaporationSourceTermsSharp.html',1,'MeltPoolDG::Evaporation']]],
+  ['evaporationthicknessintegrationdata_14',['EvaporationThicknessIntegrationData',['../structMeltPoolDG_1_1Evaporation_1_1EvaporationThicknessIntegrationData.html',1,'MeltPoolDG::Evaporation']]],
+  ['evaporativecooling_15',['EvaporativeCooling',['../classMeltPoolDG_1_1Evaporation_1_1EvaporativeCooling.html',1,'MeltPoolDG::Evaporation']]],
+  ['exactsolution_16',['exactsolution',['../classMeltPoolDG_1_1Simulation_1_1AdvectionDiffusionDG_1_1ExactSolution.html',1,'MeltPoolDG::Simulation::AdvectionDiffusionDG::ExactSolution&lt; dim, number &gt;'],['../classMeltPoolDG_1_1Simulation_1_1ReinitCircle_1_1ExactSolution.html',1,'MeltPoolDG::Simulation::ReinitCircle::ExactSolution&lt; dim, number &gt;'],['../classMeltPoolDG_1_1Simulation_1_1ReinitCircleDG_1_1ExactSolution.html',1,'MeltPoolDG::Simulation::ReinitCircleDG::ExactSolution&lt; dim, number &gt;']]],
+  ['externalfluidforcesrighthandsidecontribution_17',['ExternalFluidForcesRightHandSideContribution',['../structMeltPoolDG_1_1Flow_1_1ExternalFluidForcesRightHandSideContribution.html',1,'MeltPoolDG::Flow']]]
 ];
